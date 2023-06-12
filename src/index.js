84893c3e5d6e6aa0fadf3c3bf65b8d357b6dfa0c
Rammbest <igor-kutsenko@ukr.net>
2023-06-12 12:53:19 +0300

    add message totalhits, error, not found img and btn loadmore - show and hide

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ import axios from "axios";
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadBtn = document.querySelector('.load-more');
+const input = document.querySelector('.search-input');
+const footer = document.querySelector('.footer');
+const title = document.querySelector('.search-title')
 
 let lightbox = new SimpleLightbox('.gallery a',
     {
@@ -37,9 +40,25 @@ let name;
 async function createGallery(event) {
   event.preventDefault();
   gallery.innerHTML = '';
+  footer.innerHTML = "";
   pageNumber = 1;
-  name = event.target.searchQuery.value;
+  name = event.target.searchQuery.value.trim();
   const searchResult = await fetchImages(name, pageNumber);
+
+  if (!name) {
+    
+    title.textContent = "";
+        loadBtn.classList.add('is-hidden');
+
+    Notiflix.Notify.info('Enter data to search!', {
+        position: 'center-center',
+        width: '340px',
+    });
+
+    
+    return;
+  }
+
 
   if (searchResult.hits.length === 0) {
     Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.")
@@ -49,12 +68,18 @@ async function createGallery(event) {
       
       
       const markup = await renderGalleryCard(searchResult.hits);
-      
+      title.textContent = `Hooray! We found ${searchResult.totalHits} images.`
       gallery.insertAdjacentHTML('beforeend', markup);
 
-  lightbox.refresh();
+      lightbox.refresh();
+      loadBtn.classList.remove('is-hidden')
 
-      // const lightbox = new SimpleLightbox('.gallery a');
+      if (searchResult.totalHits < 40){
+        loadBtn.classList.add('is-hidden');
+      footer.insertAdjacentHTML('beforeend', `<h2 class="result-message">We're sorry, but you've reached the end of search results.</h2>`);
+        
+} 
+      
 
     } catch (error) {
       Notiflix.Notify.failure('Something went wrong!')
@@ -80,6 +105,11 @@ async function loadNextGallery(event) {
   
   lightbox.refresh();
   
-   
+    if (nextPage.hits.length < 40){
+      loadBtn.classList.add('is-hidden')
+      footer.insertAdjacentHTML('beforeend', `<h2 class="result-message">We're sorry, but you've reached the end of search results.</h2>`);
+    }
   
 }
+
+

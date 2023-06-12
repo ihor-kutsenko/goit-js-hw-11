535d7f2c2ab766b82e3cbe9be2b530cb873e20bf
Rammbest <igor-kutsenko@ukr.net>
2023-06-12 23:01:25 +0300

    add css style. final version

--- src/index.js
+++ src/index.js
@@ -52,41 +52,51 @@ async function createGallery(event) {
   if (!name) {
     
     title.textContent = "";
-        loadBtn.classList.add('is-hidden');
+    loadBtn.classList.add('is-hidden');
+    loadBtn.classList.remove('is-visible');
+
 
     Notiflix.Notify.info('Enter data to search!', {
         position: 'center-center',
         width: '340px',
     });
 
-    
     return;
   }
 
 
   if (searchResult.hits.length === 0) {
-    Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.")
+    Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.", {
+        position: 'center-center',
+        width: '340px',
+    })
     
   } else {
+
     try {
-      
-      
+            
       const markup = await renderGalleryCard(searchResult.hits);
       title.textContent = `Hooray! We found ${searchResult.totalHits} images.`
       gallery.insertAdjacentHTML('beforeend', markup);
 
       lightbox.refresh();
-      loadBtn.classList.remove('is-hidden')
+      loadBtn.classList.remove('is-hidden');
+      loadBtn.classList.add('is-visible')
+
 
       if (searchResult.totalHits < 40){
         loadBtn.classList.add('is-hidden');
-      footer.insertAdjacentHTML('beforeend', `<h2 class="result-message">We're sorry, but you've reached the end of search results.</h2>`);
+        loadBtn.classList.remove('is-visible');
+        footer.insertAdjacentHTML('beforeend', `<h2 class="result-message">We're sorry, but you've reached the end of search results.</h2>`);
         
-} 
+        } 
       
 
     } catch (error) {
-      Notiflix.Notify.failure('Something went wrong!')
+      Notiflix.Notify.failure('Something went wrong!', {
+        position: 'center-center',
+        width: '340px',
+    })
     }
 
   }
@@ -101,7 +111,7 @@ async function loadNextGallery(event) {
   event.preventDefault();
   
   pageNumber += 1;
-   const nextPage = await fetchImages(name, pageNumber);
+  const nextPage = await fetchImages(name, pageNumber);
   
   const nextMarkup = await renderGalleryCard(nextPage.hits);
   gallery.insertAdjacentHTML('beforeend', nextMarkup);
@@ -112,7 +122,8 @@ async function loadNextGallery(event) {
   lightbox.refresh();
   
     if (nextPage.hits.length < 40){
-      loadBtn.classList.add('is-hidden')
+      loadBtn.classList.add('is-hidden');
+      loadBtn.classList.remove('is-visible');
       footer.insertAdjacentHTML('beforeend', `<h2 class="result-message">We're sorry, but you've reached the end of search results.</h2>`);
     }
   
--- src/renderGalleryCard.js
+++ src/renderGalleryCard.js
@@ -2,23 +2,23 @@ export default function renderGalleryCard(photos) {
   return photos.map(({ webformatURL, largeImageURL, likes, tags, views, comments, downloads }) => {
     return `<a href='${largeImageURL}' class="card-link ">
     <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="330px" height="220px" />
+  <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy"  />
   <div class="info">
     <p class="info-item">
       <b>Likes</b>
-      <b>${likes}</b>
+      ${likes}
     </p>
     <p class="info-item">
       <b>Views</b>
-      <b>${views}</b>
+      ${views}
     </p>
     <p class="info-item">
       <b>Comments</b>
-      <b>${comments}</b>
+      ${comments}
     </p>
     <p class="info-item">
       <b>Downloads</b>
-      <b>${downloads}</b>
+      ${downloads}
     </p>
   </div>
 </div>
